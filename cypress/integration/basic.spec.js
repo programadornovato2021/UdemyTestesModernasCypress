c1f95d076e522404860b5e6e36f322289d9ac6ff
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-03-11 00:55:50 -0300

    Create basic.spec.js

---
+++ cypress/integration/basic.spec.js
@@ -0,0 +1,20 @@
+///<reference types="cypress" />
+
+describe('Cypress basics', () => {
+
+    it('Should visit a page and assert title', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        
+        //const title = cy.title()
+        //console.log(title)
+
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain', 'Campo de Treinamento')
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
+
+    });
+    
+});
