00fbbb7e35e0cb9dea903a165f227518ded75b9a
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-03-13 13:30:09 -0300

    Update helpers.spec.js

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -1,9 +1,30 @@
 ///<reference types="cypress" />
 
+
+
 describe('Helpers', () => {
     it('wrap', () => {
         const obj = { nome: 'user', idade: 20}
         expect(obj).to.have.property('nome')
+        cy.wrap(obj).should('have.property', 'nome')
+
+         cy.visit('https://wcaquino.me/cypress/componentes.html')
+        
+         const promise = new Promise((resolve, reject) =>{
+            setTimeout(()=>{
+                 resolve(10)
+ 
+            }, 500)
+ 
+          })
+
+        
+    });
+
+    it('Its...', () => {
+        const obj = { nome: 'User', idade: 20}
+        cy.wrap(obj).should('have.property', 'nome', 'User')
+        cy.wrap(obj).its('nome').should('be.equal', 'User')
     });
 
 });
