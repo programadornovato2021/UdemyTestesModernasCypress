8da135eb8a1fb2019da20901edffea64d70ffe65
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-03-13 22:18:45 -0300

    Update functional.spec.js

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -11,10 +11,21 @@ describe('Should test at a functional level', () => {
         cy.get('.toast').should('contain',('Bem vindo'))
     });
 
-    it('login', () => {
+    it('Should create an account', () => {
         
-        
+        cy.get('[data-test="menu-settings"]').click()
+        cy.get('[href="/contas"]').click()
+        cy.get('[data-test="nome"]').type('Conta de teste')
+        cy.get('.btn').click()
+        cy.get('.toast-success').should('contain', 'Conta inserida com sucesso')
     });
    
-    
+    it('Should update an account', () => {
+        cy.get(':nth-child(2) > :nth-child(2) > .fa-edit').click()
+        cy.get('.btn > .far').click()
+        .type('Conta alterada')
+        cy.get('.btn').click()
+        cy.get('.toast-success').should('contain', 'Conta atualizada com sucesso')
+
+    });
 })
