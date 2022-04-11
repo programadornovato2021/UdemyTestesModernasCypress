36a8dedb17ebda91ace3a6e405d1f90788bace21
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-04-10 21:02:43 -0300

    jj

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -1,6 +1,7 @@
 ///<reference types="cypress" />
 
 import loc from '../support/locators'
+import '../support/commandsContas'
 
 describe('Should test at a functional level', () => {
     
@@ -16,11 +17,12 @@ describe('Should test at a functional level', () => {
     });
 
     it('Should create an account', () => {
-        
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta de teste')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+        cy.inserirConta('Conta de teste')
+        //cy.get(loc.MENU.SETTINGS).click()
+        //cy.get(loc.MENU.CONTAS).click()
+        //cy.get(loc.CONTAS.NOME).type('Conta de teste')
+        //cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     });
    
