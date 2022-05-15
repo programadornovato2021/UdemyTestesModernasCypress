305a223f3085b7337c15be627866556557102eb9
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-05-14 21:44:56 -0300

    a

---
+++ cypress/integration/teste.spec.js
@@ -0,0 +1,13 @@
+///<reference types="cypress" />
+///<reference types="cypress" />
+
+describe('', () => {
+
+    it('teste de cadastro na renner', () => {
+        
+        cy.visit(https://www.lojasrenner.com.br/'')
+        cy.get('')
+    });
+
+    
+});
--- cypress/integration/teste1.spec.js
+++ cypress/integration/teste1.spec.js
@@ -1,4 +1,4 @@
-///<reference types="cypress" />
+
 
 describe('Criando um teste rapido', () => {
     
@@ -7,6 +7,8 @@ describe('Criando um teste rapido', () => {
         cy.visit('https://www.magazineluiza.com.br/')
         cy.get('[data-testid="input-search"]').type('armario')
         cy.get('[data-testid="search-submit"]').click()
+        cy.get(':nth-child(5) > [data-testid="product-card-container"] > [data-testid="product-card-content"] > [data-testid="product-title"]').click()
+        
 
 
 
