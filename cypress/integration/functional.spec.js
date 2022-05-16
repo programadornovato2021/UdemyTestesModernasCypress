ba4b5a79b5bc917657c356848379d60efeed979e
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-05-15 22:40:13 -0300

    Qualidade

--- cypress/integration/functional.spec.js
+++ cypress/integration/functional.spec.js
@@ -1,37 +1,13 @@
-///<reference types="cypress" />
+///<reference types="cypress"/>
 
-import loc from '../support/locators'
-import '../support/commandsContas'
-
-describe('Should test at a functional level', () => {
+describe('Deve testar a nivel funcional', () => {
     
-
     before(() => {
-        cy.login('programador21@gmail.com', 'maluco123')
-        cy.resetApp()
-        //cy.visit('https://barrigareact.wcaquino.me/')
-        //cy.get(loc.LOGIN.USER).type('programador21@gmail.com')
-        //cy.get(loc.LOGIN.PASSOWORD).type('maluco123')
-        //cy.get(loc.LOGIN.BTN_LOGIN).click()
-        //cy.get(loc.MESSAGE).should('contain',('Bem vindo'))
+        cy.visit('https://barrigareact.wcaquino.me/login')
+    });
+    
+    it('login', () => {
+        
     });
 
-    it('Should create an account', () => {
-        cy.acessarMenuConta()
-        cy.inserirConta('Conta de teste')
-        //cy.get(loc.MENU.SETTINGS).click()
-        //cy.get(loc.MENU.CONTAS).click()
-        //cy.get(loc.CONTAS.NOME).type('Conta de teste')
-        //cy.get(loc.CONTAS.BTN_SALVAR).click()
-        cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
-    });
-   
-    it('Should update an account', () => {
-        cy.get(loc.CONTAS.XP_BTN_ALTERAR).click()
-        cy.get(loc.CONTAS.NOME)
-        .type('Conta alterada')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
-        cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso')
-
-    });
-})
+});
